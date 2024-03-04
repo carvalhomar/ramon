e78d6f318cf2814b00fe037722aa95138dfc8d82
Marcelo Carvalho de moura <marcelocarvalhodemoura@MacBook-Pro-de-Marcelo.local>
2024-03-04 20:38:04 -0300

    curriculum atualizado

--- resources/views/page/curriculum.blade.php
+++ resources/views/page/curriculum.blade.php
@@ -14,9 +14,9 @@
                     <p class="text-left curriculum-descript"><b>Currículo resumido:</b> Médico pela Universidade Federal de Santa Catarina (2005),
                         Psiquiatra (2011) e Psicoterapeuta (2012) pela Fundação Universitária Mário Martins,
                         Mestre em Ciências da Saúde (área de concentração: Terapêutica Clínica)
-                        pela Universidade Federal de Ciências da Saúde de Porto Alegre (2014) e
+                        pela Universidade Federal de Ciências da Saúde de Porto Alegre (2014),
                         Membro do Instituto de Psicanálise da Sociedade Brasileira de Psicanálise de Porto Alegre,
-                        filiada à <i>International Psychoanalytical Association</i> - IPA (2015).</p>
+                        filiada à <i>International Psychoanalytical Association</i>, e Escritor.</p>
 
                     <p class="text-left curriculum-descript"><b>Currículo completo:</b> clicando<a target="_blank" href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4363072P8&tokenCaptchar=03AGdBq25xwoveRrHphUcDuvxq8qRM75OcqoqCvgXNiHr4ItegT_HZg9gFCq1tdHDFSHv3bjwHO-5OKYvP60FWkPa6D138r5QLDlAoJv2iWu4RskBF66m7kgOKXVB2unqfD5hVjtwWd6oqHVG-FgPOM0EAF-nxQy9gkG5QtbWvWt8AOMzDWttHURwBCBstO_NHkjm9duO3cfAtSf6cFsHgbDRS7IHScUORrBQQ70EkG31q9aFVB5ibV3SHmA8rQx-jGiWjHkD0T1HSfgAdEyBV3DoiULXg075Q-TMLwUO1kw1Wwx_zdvS9f5IavrAiBEpMUbWOgKd7B1AEwwQVrm-c38LeXKzLqLh0TLPxXEWwhNyIO_8dtRZWCk9o9rJ9Ak7nWl5gxy1cRDdXgJudKfuG2YtPAXvQSU"> aqui.</a></p>
                 </div>
